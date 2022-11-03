35ad7f6e91f67ce3a543b9642ca0731c5551a18b
GabeKL22 <113698242+GabeKL22@users.noreply.github.com>
2022-11-03 18:56:39 -0400

    Add files via upload

---
+++ proj1_processes/processes_GLeffew.c
@@ -0,0 +1,144 @@
+/*
+Author:            Gabriel Leffew
+Major:             Computer Science
+Creation Date:     09/17/22
+Due Date:          09/22/22
+Course:            CSC328
+Professor Name:    Dr. Frye
+Assignment:        Processes HW#6
+Filename:          processes_GLeffew.c
+Purpose:           This program will accept a command line 
+                   argument stating the number of processes, 
+                   the program will then run that number of 
+                   sequential, then concurrent processes.
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int x;
+
+
+/*
+Function name: check_input
+Description:   check the command line arguments (if between 1-10 and only one argument)
+Parameters:    int num: amount of command line arguments
+               char *a: amount of processes
+Return Value:  amt: amount of processes to create
+*/
+int check_input(int num, char *a) {
+
+    int amt = atoi(a);
+    //check if between 1 and 10
+    if (amt < 1 || amt > 10) {
+        printf("Number must be between 1 and 10.\n");
+    }
+    //check command line arguments
+    if (num == 2) {
+        return amt;
+    } else if (num > 2) {
+        printf("Too many arguments.\n");
+    } else {
+        printf("Please enter a number of processes 1-10.\n");
+    }
+
+    return amt;
+}
+
+/*
+Function name: child
+Description:   print output when child is executed, increment variables
+Parameters:    int num: variable to increment
+               int *nump: pointer variable to increment
+               int i: the current process index
+Return Value:  none - void function
+*/
+void child(int num, int *nump, int i) {
+    printf("  BEFORE increment: Child number %d with PID of %d: x = %d, num = %d, nump = %d\n", i+1, getpid(), x, num, *nump);
+    x = x + 50;
+    num++;
+    *nump=(*nump)+1;
+    //sleep(1);
+    printf("  --AFTER increment: Child number %d with PID of %d: x = %d, num = %d, nump = %d\n", i+1, getpid(), x, num, *nump);
+
+}
+/*
+Function name: parent
+Description:   waits for child to finish, then prints when it has ended
+Parameters:    int status: unassigned variable (NULL)
+Return Value:  none - void function 
+*/
+void parent(int status) {
+    int rv = wait(&status); //wait for child
+    if (rv == -1) {
+        perror("wait");
+        exit(1);
+    }
+    printf("    [Child PID: %d has ended.]\n", rv);
+}
+/*
+Function name: main
+Description:   call functions, and initialize variables, also start
+               a certain number of sequential, and concurrent processes
+Parameters:    int argc: amount of command line arguments
+               char *argv[]: charecter array of inputs 
+Return Value:  0
+*/
+int main (int argc, char *argv[]) {
+
+    //create varibles to increment
+    int numProcs = check_input(argc, argv[1]);
+    x = 500;
+    int num = 5;
+    int *nump = (int *)malloc(sizeof(int));
+    *nump = 10;
+    int status;
+
+    printf("\nThe program is creating sequential processes -- numProcs: %d | x: %d | num: %d | nump: %d\n\n", numProcs, x, num, *nump);
+
+    //create numProcs sequential processes
+    for (int i = 0; i < numProcs; i++) {
+
+        int pid = fork(); // call fork pid is processID
+
+        if (pid < 0) {
+            perror("fork");
+            exit(1);
+        } else if (pid == 0) {
+            child(num, nump, i); //call child
+            exit(0);
+        }
+        parent(status); //call parent function
+    }
+
+    //reset
+    x = 500;
+    num = 5;
+    *nump = 10;
+
+    //create numProcs concurrent processes
+    printf("\nThe program is creating concurrent processes -- numProcs: %d | x: %d | num: %d | nump: %d\n\n", numProcs, x, num, *nump);
+    for (int i = 0; i < numProcs; i++){
+
+        int pid = fork(); // call fork pid is processID
+
+        if (pid < 0) {
+            perror("fork");
+            exit(1);
+        } else if (pid == 0) {
+            child(num, nump, i); // call child 
+            sleep(1);
+            exit(0);
+        }
+    }
+
+    for (; numProcs > 0; numProcs--) {
+        parent(status); //call parents
+    }
+
+    printf("Program finished.\n");
+
+    return 0;
+}
---
+++ proj3_pipes/pipes_GLeffew.c
@@ -0,0 +1,163 @@
+/*
+Author:            Gabriel Leffew
+Major:             Computer Science
+Creation Date:     10/15/22
+Due Date:          10/20/22
+Course:            CSC328
+Professor Name:    Dr. Frye
+Assignment:        pipes HW#8
+Filename:          pipes_GLeffew.c
+Purpose:           This program utilitzes pipes 
+                   to allow child and parent processes
+                   to read and write from eachother.
+*/
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <time.h>
+
+#define MAXLINE 512
+#define READ      0
+#define WRITE     1
+
+/*
+Function name: child
+Description:   write to the parent process, and read from the 
+               parent process.
+Parameters:    char *line - malloc() to get a void pointer allocate on heap
+               int p2c[] - parent to child file descripter
+               int c2p[] - child to parent fd
+Return Value:  none, void function 
+*/
+void child(char *line, int p2c[], int c2p[]) {
+    close(p2c[WRITE]);
+    close(c2p[READ]);
+    int x, y;
+    if (read(p2c[READ], &x, sizeof(x)) == -1) { 
+        perror("Error reading from pipe in child.\n");
+        exit(1); 
+    }
+    printf("Child received from pipe: %d\n", x);
+    if (read(p2c[READ], &y, sizeof(x)) == -1) { 
+        perror("Error reading from pipe in child.\n");
+        exit(1); 
+    }
+    printf("Child received from pipe: %d\n", y);
+    sleep(1);
+
+    if (y == (x * getpid())) { // check if approved or denied
+        if (write(c2p[WRITE], "Approved", MAXLINE) == -1) { 
+            perror("Error writing to pipe in child.\n");
+            exit(1); 
+        }
+        printf("Child sending to pipe: Approved\n");
+    } else {
+        if (write(c2p[WRITE], "Denied", MAXLINE) == -1) { 
+            perror("Error writing to pipe in child.\n");
+            exit(1); 
+        }
+        printf("Child sending to pipe: Denied\n");
+    }
+    
+    if (read(p2c[READ], line, MAXLINE) == -1) { 
+        perror("Error reading from pipe in child.\n");
+        exit(1); 
+    }
+    printf("Child received from pipe: %s\n", line);
+    close(p2c[READ]);
+    close(c2p[WRITE]);
+    if (line = "BYE") {
+        exit(1);
+    }
+}
+/*
+Function name: parent
+Description:   write to the child process, and read from the 
+               child process.
+Parameters:    char *line - malloc() to get a void pointer allocate on heap
+               pid_t pid - process id
+               int p2c[] - parent to child file descripter
+               int c2p[] - child to parent fd
+Return Value:  none, void function 
+*/
+void parent(char *line, pid_t pid, int p2c[], int c2p[]) {
+    close(p2c[READ]);
+    close(c2p[WRITE]);
+    srand(time(NULL));
+    int rand_num = rand() % 100;
+    if (write(p2c[WRITE], &rand_num, sizeof(rand_num)) == -1) { 
+        perror("Error writing to pipe from parent.\n");
+        exit(1); 
+    }
+    printf("Parent sending to pipe: %d\n", rand_num);
+    sleep(1); // sleep to send and receive first
+    int product = rand_num * pid;
+    if (write(p2c[WRITE], &product, sizeof(product)) == -1) { 
+        perror("Error writing to pipe from parent.\n");
+        exit(1); 
+    }
+    printf("Parent sending to pipe: %d\n", product);
+    char size[10];
+    if (read(c2p[READ], line, MAXLINE) == -1) { 
+        perror("Error reading from pipe in child.\n");
+        exit(1); 
+    }
+    printf("Parent received from pipe: %s\n", line);
+    //determine output based on value from child
+    if (line = "Approved") {
+        printf("Parent: Thanks for playing!\n");
+    } else {
+        printf("Parent: Wrong. Please play again\n");
+    }
+    //send BYE
+    if (write(p2c[WRITE], "BYE", MAXLINE) == -1) { 
+        perror("Error writing to pipe from parent.\n");
+        exit(1); 
+    }
+    printf("Parent sending to pipe: BYE\n");
+    int rv = wait(NULL);
+    if (rv == -1) {
+      perror("Error in wait: ");
+      exit(-1);
+    }
+    close(p2c[WRITE]);
+    close(c2p[READ]);
+
+}
+
+/*
+Function name: main
+Description:   create twp pipes and fork() a process
+               calling parent() and child() functions
+Parameters:    int argc: amount of command line arguments
+               char *argv[]: charecter array of inputs 
+Return Value:  0
+*/
+int main(int argc, char* argv[]) {
+
+    int p2c[2], c2p[2]; // two pipes, for each direction
+    pid_t pid;
+    char *line=(char *)malloc(sizeof(char)*MAXLINE);
+
+    if (pipe(p2c) == -1) {  //parent to child
+        perror("An error occured creating the parent to child pipe.\n");
+        exit(1);
+    }
+    if (pipe(c2p) == -1) {  //child to parent
+        perror("An error occured creating the child to parent pipe.\n");
+        exit(1);
+    }    
+    if ((pid = fork()) < 0) {
+        perror("An error occured creating a process with fork().\n");
+    } else if (pid == 0) {  //do child stuff
+        child(line, p2c, c2p);
+    } else {  //do parent stuff
+        parent(line, pid, p2c, c2p);
+    }
+
+    free(line); //free heap
+    return 0;
+}
